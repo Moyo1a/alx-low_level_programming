51413374ae77d6df598b69dfd67651f13ebcf6f9
Moyo1a <maykaydesigns485@gmail.com>
2023-02-20 20:12:54 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+*main -the main content{
+*Return:the answer is zero (success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
