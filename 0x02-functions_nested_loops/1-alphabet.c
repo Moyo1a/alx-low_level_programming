489e873b1398402b57eaae2d5a90e5fc529f7530
Moyo1a <maykaydesigns485@gmail.com>
2023-02-24 08:57:16 -0800

    print alphabets in lowercase a to z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_alphabet - this is the entry point of lowercase alphabets
+* Return: always success (0)
+*/
+void print_alphabet(void)
+{
+	char c = 'a';
+		while (c < = 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+}
+
+
+
+
