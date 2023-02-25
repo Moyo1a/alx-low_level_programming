e1ba606962d002db00c8b0ece9e32a2d87a71108
Moyo1a <maykaydesigns485@gmail.com>
2023-02-25 10:18:28 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
-* print_alphabet_x10 - prints 10 timesthe alphabet in lowercase
+* print_alphabet_x10 - Prints lowercase case alphabets x10
 * Return: always success (0)
 */
 void print_alphabet_x10(void)
 {
 	int i;
 	int j;
+
 	for (j = 1; i <= 10; i++)
 	{
 		for (j = 97; j <= 122; j++)
@@ -15,3 +16,4 @@ void print_alphabet_x10(void)
 		}
 		_putchar('\n');
 	}
+}
