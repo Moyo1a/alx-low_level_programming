aea71c34d4e5328665da88a0a904a68ed07edc31
Moyo1a <maykaydesigns485@gmail.com>
2023-02-27 03:48:54 -0800

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * jack_bauer - prints 24 hours make a day
-* Return: hours and minutes
+* Return: always 0
 */
 void jack_bauer(void)
 {
@@ -14,7 +14,7 @@ void jack_bauer(void)
 		{
 			_putchar(i / 10 + '0');
 			_putchar(i % 10 + '0');
-			_putchar(":");
+			_putchar(':');
 			_putchar(j / 10 + '0');
 			_putchar(j % 10 + '0');
 			_putchar('\n');
