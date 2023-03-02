6d1a12d8ae9ef5898784aec64a739c337836bec9
Moyo1a <maykaydesigns485@gmail.com>
2023-03-02 10:16:53 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,21 +5,42 @@
 */
 void times_table(void)
 {
-	int a, b, mul, x, chk;
+	int a, b, c;
 
 		for (a = 0; a <= 9; a++)
 		{
-			for (b = 0; x = 1; b <= 9; b++; x++)
+			for (b = 0; b <= 9; b++)
 			{
-				if (chk > 9 && b != 9)
-				mul = a * b
-					chk = a * x;
-					_putchar("%d, ", mul);
-				else if (b == 9)
-					_putchar("%d", mul);
+				c = a * b;
+				if ((c / 10) == 0)
+				{
+				if (b == 0)
+				{
+					_putchar('0');
+				}
+				if (b != 0)
+				{
+					_putchar(' ');
+					_putchar((c % 10) + '0');
+				}
+				if (b < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}				}
 				else
-					_putchar("%d,  ", mul);
+				{
+					_putchar((c / 10) + '0');
+					_putchar((c % 10) + '0');
+					if (b < 9)
+					{
+						_putchar(',');
+						_putchar(' ');
+					}
+				}
 			}
-			_putchar('\n');
 		}
 }
+
+
+
