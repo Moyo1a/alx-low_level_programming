0726e6583e4734fef8179bde108b5e4464b21ddd
Moyo1a <maykaydesigns485@gmail.com>
2023-03-04 00:22:43 -0800

    printing numbers within 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,3 +1,39 @@
 #include "main.h"
+#include <stdio.h>
 /**
-* print_to_98 - 
+* print_to_98 - get the numbers from 0 - 98
+*
+* @n: set of integer numbers
+* Return: always 0
+*/
+void print_to_98(int n)
+{
+	int i;
+
+	if (n >= 0 && n <= 98)
+	{
+	for (i = n; i <= 98, i++)
+	{
+		printf("%d, ", i);
+	}
+		print("\n");
+	}
+	else if (n < 0)
+	{
+	for (i = n; i <= 98; i++)
+		{
+		printf("%d, ", i);
+		}
+		printf("\n");
+	}
+	else if (n > 98)
+	{
+	for (i = n; i >= 98; i--)
+	{
+		printf("%d, ", i);
+	}
+		printf("\n");
+	}
+	else if (n == 98)
+		printf("%d, ", n);
+}
