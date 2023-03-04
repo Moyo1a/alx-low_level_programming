11655b595325e3a9fc75211680aa4498a171e1b9
Moyo1a <maykaydesigns485@gmail.com>
2023-03-04 02:50:45 -0800

    signs of integers

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,28 +1,27 @@
 #include "main.h"
 /**
-* print_sign - this prints the sign of a number if n is greater, less or equal to zero
-* @n:n is an input number (integer)
-* Return:shows 1 if greater than 0, 0 if n is zero, -1 if less than 0
+* print_sign -declarations if the input numbers
+* @n: numbers as integers will print the signs
+* Return: if n > 0, if n < 0, if n == 0, print the sign
 */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(+);
+		_putchar(43);
 		return (1);
 	}
 	else if (n < 0)
 	{
-		_putchar(-);
+		_putchar(45);
 		return (-1);
 	}
 	else
 	{
-		_putchar(0 + '0');
+		_putchar(48);
 		return (0);
 	}
 	_putchar('\n');
 }
-
 
 
